8c52b538b8747544d2bc5a00b60a1879ab66a424
Alejandro Robles <omara.robles@softtek.com>
2025-11-13 17:56:33 -0600

    Add actions to goBack and popToTop

--- 04-navigation-app/src/presentation/screens/settings/SettingsScreen.tsx
+++ 04-navigation-app/src/presentation/screens/settings/SettingsScreen.tsx
@@ -1,9 +1,20 @@
 import { Text, View } from 'react-native';
+import { globalStyles } from '../../theme/theme';
+import { StackActions, useNavigation } from '@react-navigation/native';
+import { PrimaryButton } from '../../components/shared/PrimaryButton';
 
 export const SettingsScreen = () => {
+  const navigator = useNavigation();
+
   return (
-    <View>
-      <Text> Settings Screen</Text>
+    <View style={globalStyles.container}>
+      <Text style={{ marginBottom: 10 }}> Settings Screen</Text>
+
+      <PrimaryButton label="Regresar" onPress={() => navigator.goBack()} />
+      <PrimaryButton
+        label="Regresar al Home"
+        onPress={() => navigator.dispatch(StackActions.popToTop)}
+      />
     </View>
   );
 };
