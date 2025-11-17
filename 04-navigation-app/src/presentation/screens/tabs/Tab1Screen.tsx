aedb428d21f6552d58750acf23b011d5d9b38983
Alejandro Robles <omara.robles@softtek.com>
2025-11-17 14:10:15 -0600

    Create hamburger menu

--- 04-navigation-app/src/presentation/screens/tabs/Tab1Screen.tsx
+++ 04-navigation-app/src/presentation/screens/tabs/Tab1Screen.tsx
@@ -1,8 +1,10 @@
 import { Text, View } from 'react-native';
+import { HamburgerMenu } from './HamburgerMenu';
 
 export const Tab1Screen = () => {
   return (
     <View>
+      <HamburgerMenu />
       <Text> Tab1Screen</Text>
     </View>
   );
