bac20730c1dfd9f990029c7547a1db45382dba89
Alejandro Robles <omara.robles@softtek.com>
2025-11-17 18:29:31 -0600

    Add icons for android

--- 04-navigation-app/src/presentation/screens/tabs/Tab1Screen.tsx
+++ 04-navigation-app/src/presentation/screens/tabs/Tab1Screen.tsx
@@ -1,11 +1,13 @@
 import { Text, View } from 'react-native';
 import { HamburgerMenu } from './HamburgerMenu';
+import { Ionicons } from '@react-native-vector-icons/ionicons';
 
 export const Tab1Screen = () => {
   return (
     <View>
       <HamburgerMenu />
       <Text> Tab1Screen</Text>
+      <Ionicons name="rocket" size={30} />
     </View>
   );
 };
