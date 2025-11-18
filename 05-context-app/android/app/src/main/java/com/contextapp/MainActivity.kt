83218e76f4ec4c0dee530957d5637edc2fef7dea
Alejandro Robles <omara.robles@softtek.com>
2025-11-18 12:34:43 -0600

    Add bottom tabs

--- 05-context-app/android/app/src/main/java/com/contextapp/MainActivity.kt
+++ 05-context-app/android/app/src/main/java/com/contextapp/MainActivity.kt
@@ -1,4 +1,6 @@
 package com.contextapp
+import android.os.Bundle
+import com.swmansion.rnscreens.fragment.restoration.RNScreensFragmentFactory
 
 import com.facebook.react.ReactActivity
 import com.facebook.react.ReactActivityDelegate
@@ -6,6 +8,11 @@ import com.facebook.react.defaults.DefaultNewArchitectureEntryPoint.fabricEnable
 import com.facebook.react.defaults.DefaultReactActivityDelegate
 
 class MainActivity : ReactActivity() {
+
+  override fun onCreate(savedInstanceState: Bundle?) {
+    supportFragmentManager.fragmentFactory = RNScreensFragmentFactory()
+    super.onCreate(savedInstanceState)
+  }
 
   /**
    * Returns the name of the main component registered from JavaScript. This is used to schedule
--- 05-context-app/src/Main.tsx
+++ 05-context-app/src/Main.tsx
@@ -1,9 +1,12 @@
-import { Text, View } from 'react-native';
+import { Text } from 'react-native';
+import { BottomTabNavigator } from './presentation/navigators/BottomTabNavigator';
+import { NavigationContainer } from '@react-navigation/native';
 
 export const Main = () => {
   return (
-    <View>
+    <NavigationContainer>
       <Text>Hola Mundo!!!</Text>
-    </View>
+      <BottomTabNavigator />
+    </NavigationContainer>
   );
 };
