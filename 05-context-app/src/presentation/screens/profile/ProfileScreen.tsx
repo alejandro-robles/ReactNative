d91044e15bc7112438269e2c36defbfbe7c5aef5
Alejandro Robles <omara.robles@softtek.com>
2025-11-18 13:26:27 -0600

    Change data on the store

--- 05-context-app/src/presentation/screens/profile/ProfileScreen.tsx
+++ 05-context-app/src/presentation/screens/profile/ProfileScreen.tsx
@@ -1,15 +1,37 @@
-import { Text, View } from 'react-native';
+import { Pressable, Text, View } from 'react-native';
 import { styles } from '../../../config';
 import { useProfileStore } from '../../store/profile-store';
 
 export const ProfileScreen = () => {
   const name = useProfileStore(state => state.name);
   const email = useProfileStore(state => state.email);
+  const changeProfile = useProfileStore(state => state.changeProfile);
 
   return (
     <View style={styles.container}>
       <Text style={styles.title}>{name}</Text>
       <Text style={styles.title}>{email}</Text>
+
+      <Pressable
+        style={styles.primaryButton}
+        onPress={() => useProfileStore.setState({ name: 'Fernando Herrera' })}
+      >
+        <Text>Cambiar nombre</Text>
+      </Pressable>
+      <Pressable
+        style={styles.primaryButton}
+        onPress={() =>
+          useProfileStore.setState({ email: 'fernando.herrera@google.com' })
+        }
+      >
+        <Text>Cambiar email</Text>
+      </Pressable>
+      <Pressable
+        style={styles.primaryButton}
+        onPress={() => changeProfile('John Doe', 'john.doe@google.com')}
+      >
+        <Text>Regresar a John</Text>
+      </Pressable>
     </View>
   );
 };
