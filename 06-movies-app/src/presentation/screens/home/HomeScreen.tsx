a2bf90178ff27f4fec30a89e13a5614416e05979
Alejandro Robles <omara.robles@softtek.com>
2025-11-19 17:27:53 -0600

    Create custom hhok

--- 06-movies-app/src/presentation/screens/home/HomeScreen.tsx
+++ 06-movies-app/src/presentation/screens/home/HomeScreen.tsx
@@ -1,6 +1,8 @@
 import { Text, View } from 'react-native';
+import { useMovies } from '../../hooks/useMovies';
 
 export const HomeScreen = () => {
+  const {} = useMovies();
   return (
     <View>
       <Text style={{ paddingTop: 50 }}>HomeScreen</Text>
