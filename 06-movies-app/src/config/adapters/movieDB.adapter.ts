21deb078b400dc124332f0bc8dff5cf21d58c202
Alejandro Robles <omara.robles@softtek.com>
2025-11-24 17:37:39 -0600

    Add environment varibales to react

--- 06-movies-app/src/config/adapters/movieDB.adapter.ts
+++ 06-movies-app/src/config/adapters/movieDB.adapter.ts
@@ -1,9 +1,10 @@
+import { THE_MOVIE_DB_KEY } from '@env';
 import { AxiosAdapter } from './http/axios.adapter';
 
 export const movieDBFetcher = new AxiosAdapter({
   baseUrl: 'https://api.themoviedb.org/3/movie/',
   params: {
-    api_key: 'f7918a8e9f0abc2ebf4f18ba417255d5',
+    api_key: THE_MOVIE_DB_KEY,
     language: 'es',
   },
 });
