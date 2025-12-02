dbeaa4643c77d253a4651c8d750645499a08b033
Alejandro Robles <omara.robles@softtek.com>
2025-12-02 13:12:30 -0600

    Add Bounce animation

--- 07-components-app/src/presentation/screens/animations/Animation101Screen.tsx
+++ 07-components-app/src/presentation/screens/animations/Animation101Screen.tsx
@@ -1,11 +1,26 @@
-import { Animated, Pressable, StyleSheet, Text, View } from 'react-native';
+import {
+  Animated,
+  Easing,
+  Pressable,
+  StyleSheet,
+  Text,
+  View,
+} from 'react-native';
 import { colors } from '../../../config/theme/theme';
 import { useRef } from 'react';
 
 export const Animation101Screen = () => {
   const animatedOpacity = useRef(new Animated.Value(0)).current;
+  const animatedTop = useRef(new Animated.Value(-100)).current;
 
   const fadeIn = () => {
+    Animated.timing(animatedTop, {
+      toValue: 0,
+      duration: 700,
+      useNativeDriver: true,
+      // easing: Easing.elastic(1),
+      easing: Easing.bounce,
+    }).start(() => console.log('Animation ended'));
     Animated.timing(animatedOpacity, {
       toValue: 1,
       duration: 300,
@@ -18,7 +33,7 @@ export const Animation101Screen = () => {
       toValue: 0,
       duration: 300,
       useNativeDriver: true,
-    }).start(() => console.log('Animation started'));
+    }).start(() => animatedTop.resetAnimation());
   };
 
   return (
@@ -28,6 +43,11 @@ export const Animation101Screen = () => {
           styles.purpleBox,
           {
             opacity: animatedOpacity,
+            transform: [
+              {
+                translateY: animatedTop,
+              },
+            ],
           },
         ]}
       />
