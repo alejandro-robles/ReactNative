5f510ddab154931462ca823b71987d4cc451f957
Alejandro Robles <omara.robles@softtek.com>
2025-12-05 17:34:10 -0600

    Create separator

--- 07-components-app/src/presentation/screens/switches/SwitchScreen.tsx
+++ 07-components-app/src/presentation/screens/switches/SwitchScreen.tsx
@@ -2,6 +2,7 @@ import { CustomView } from '../../components/ui/CustomView';
 import { Card } from '../../components/ui/Card';
 import { CustomSwitch } from '../../components/ui/CustomSwitch';
 import { useState } from 'react';
+import { Separator } from '../../components/ui/Separator';
 
 export const SwitchScreen = () => {
   // const [isEnabled, setIsEnabled] = useState(false);
@@ -21,11 +22,13 @@ export const SwitchScreen = () => {
           onChange={value => setState({ ...state, isActive: value })}
           text="¿Está activo?"
         />
+        <Separator />
         <CustomSwitch
           isOn={state.isHungry}
           onChange={value => setState({ ...state, isHungry: value })}
           text="¿Tiene hambre?"
         />
+        <Separator />
         <CustomSwitch
           isOn={state.isHappy}
           onChange={value => setState({ ...state, isHappy: value })}
