7b2d71732436901fc3162319c809555cd3081bd0
Alejandro Robles <omara.robles@softtek.com>
2025-12-08 18:03:43 -0600

    Create infinity scroll with images

--- 07-components-app/src/presentation/screens/ui/InfiniteScrollScreen.tsx
+++ 07-components-app/src/presentation/screens/ui/InfiniteScrollScreen.tsx
@@ -1,8 +1,8 @@
 import { useState } from 'react';
-import { CustomView } from '../../components/ui/CustomView';
-import { Title } from '../../components/ui/Title';
-import { FlatList, Text } from 'react-native';
+import { ActivityIndicator, FlatList, Image, View } from 'react-native';
 import { colors } from '../../../config/theme/theme';
+// import ActivityIndicator from 'react-native/types_generated/Libraries/Components/ActivityIndicator/ActivityIndicator';
+// import { ActivityIndicator } from 'react-native/types_generated/index';
 
 export const InfiniteScrollScreen = () => {
   const [numbers, setNumber] = useState([0, 1, 2, 3, 4, 5]);
@@ -11,30 +11,35 @@ export const InfiniteScrollScreen = () => {
     const newArray = Array.from({ length: 5 }, (_, i) => numbers.length + i);
     setTimeout(() => {
       setNumber([...numbers, ...newArray]);
-    }, 3000);
+    }, 1500);
   };
   return (
-    <CustomView>
-      <Title text="Infinite Scroll" safe />
+    <View style={{ backgroundColor: 'black' }}>
       <FlatList
         data={numbers}
         onEndReached={loadMore}
         onEndReachedThreshold={0.6}
         keyExtractor={item => item.toString()}
-        renderItem={({ item }) => (
-          <Text
-            style={{
-              height: 300,
-              backgroundColor: colors.primary,
-              color: 'white',
-              fontSize: 50,
-            }}
-          >
-            {' '}
-            {item}
-          </Text>
+        renderItem={({ item }) => <ListItem number={item} />}
+        ListFooterComponent={() => (
+          <View style={{ height: 150, justifyContent: 'center' }}>
+            <ActivityIndicator size={40} color={colors.primary} />
+          </View>
         )}
       />
-    </CustomView>
+    </View>
+  );
+};
+
+interface ListItemProps {
+  number: number;
+}
+
+const ListItem = ({ number }: ListItemProps) => {
+  return (
+    <Image
+      source={{ uri: `https://picsum.photos/id/${number}/500/400` }}
+      style={{ height: 400, width: '100%' }}
+    />
   );
 };
