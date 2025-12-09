65d25405a609fc9ee91ec7984a4b9d642339ce4d
Alejandro Robles <omara.robles@softtek.com>
2025-12-09 16:37:55 -0600

    Create ChangeThemeScreen

---
+++ 07-components-app/src/presentation/screens/theme/ChangeThemeScreen.tsx
@@ -0,0 +1,16 @@
+import { Button } from '../../components/ui/Button';
+import { CustomView } from '../../components/ui/CustomView';
+import { Title } from '../../components/ui/Title';
+import { View } from 'react-native';
+
+export const ChangeThemeScreen = () => {
+  return (
+    <CustomView margin>
+      <Title text="Cambiar tema" safe />
+
+      <Button text="Light" onPress={() => {}} />
+      <View style={{ height: 10 }} />
+      <Button text="Dark" onPress={() => {}} />
+    </CustomView>
+  );
+};
