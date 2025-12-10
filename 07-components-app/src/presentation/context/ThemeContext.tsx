0304f94336cb35440897acf064f09d9df8ff5db5
Alejandro Robles <omara.robles@softtek.com>
2025-12-10 17:17:16 -0600

    Change theme based on the Operative System

--- 07-components-app/src/presentation/context/ThemeContext.tsx
+++ 07-components-app/src/presentation/context/ThemeContext.tsx
@@ -1,5 +1,6 @@
-import { createContext, PropsWithChildren, useState } from 'react';
+import { createContext, PropsWithChildren, useEffect, useState } from 'react';
 import { darkColors, lightColors, ThemeColors } from '../../config/theme/theme';
+import { useColorScheme } from 'react-native';
 
 type ThemeColor = 'light' | 'dark';
 
@@ -15,6 +16,16 @@ export const ThemeContext = createContext({} as ThemeContextProps);
 
 export const ThemeProvider = ({ children }: PropsWithChildren) => {
   const [currentTheme, setCurrentTheme] = useState<ThemeColor>('light');
+  const colorScheme = useColorScheme();
+
+  useEffect(() => {
+    if (colorScheme === 'dark') {
+      setCurrentTheme('dark');
+    } else {
+      setCurrentTheme('light');
+    }
+  }, [colorScheme]);
+
   const setTheme = (theme: ThemeColor) => {
     setCurrentTheme(theme);
   };
