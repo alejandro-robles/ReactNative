1d1e1d9271bcb04f5780f82b957f6a0a35c43ddc
Alejandro Robles <omara.robles@softtek.com>
2025-12-10 16:59:29 -0600

    Add react navigation theme

--- 07-components-app/src/ComponentsApp.tsx
+++ 07-components-app/src/ComponentsApp.tsx
@@ -1,20 +1,37 @@
-import { NavigationContainer } from '@react-navigation/native';
+import {
+  DarkTheme,
+  DefaultTheme,
+  NavigationContainer,
+} from '@react-navigation/native';
 import { Navigator } from './presentation/navigator/Navigator';
-import { ThemeProvider } from './presentation/context/ThemeContext';
-import { PropsWithChildren } from 'react';
+import {
+  ThemeContext,
+  ThemeProvider,
+} from './presentation/context/ThemeContext';
+import { PropsWithChildren, useContext } from 'react';
 
-const AppState = ({ children }: PropsWithChildren) => {
+const AppNavigation = ({ children }: PropsWithChildren) => {
+  const { isDark } = useContext(ThemeContext);
+
   return (
-    <NavigationContainer>
-      <ThemeProvider>{children}</ThemeProvider>
+    <NavigationContainer theme={isDark ? DarkTheme : DefaultTheme}>
+      {children}
     </NavigationContainer>
+  );
+};
+
+const AppTheme = ({ children }: PropsWithChildren) => {
+  return (
+    <ThemeProvider>
+      <AppNavigation>{children}</AppNavigation>
+    </ThemeProvider>
   );
 };
 
 export const ComponentsApp = () => {
   return (
-    <AppState>
+    <AppTheme>
       <Navigator />
-    </AppState>
+    </AppTheme>
   );
 };
